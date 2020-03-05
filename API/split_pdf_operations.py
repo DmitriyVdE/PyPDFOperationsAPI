aa35cc2945b523d8d427ea7c22b6cbd742b00b93
Dmitriy Van der Elst <dmitriy.vde@gmail.com>
2020-03-05 16:00:09 +0100

    Small changes

--- API/split_pdf_operations.py
+++ API/split_pdf_operations.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from flask_config import app
 from flask import jsonify
 from PyPDF2 import PdfFileReader, PdfFileWriter
+from zipfile import ZipFile
 
 def generate_file_name():
   filename = str(calendar.timegm(time.gmtime()))
@@ -11,13 +12,19 @@ def generate_file_name():
 
 def split_pdf(filelocation):
   iterate_over_file(filelocation)
+  zip_split_files(filelocation)
   resp = jsonify({'message' : 'File successfully uploaded.', 'file location' : filelocation})
   resp.status_code = 201
   return resp
 
 def iterate_over_file(filelocation):
+  print(filelocation)
+  #os.chdir(filelocation)
+  
   filename = filelocation.split('/')[1].split('.')[0]
-  print(filename)
+  
+  #zipped_pdfs = ZipFile(filename + '.zip', 'w')
+  
   pdf = PdfFileReader(filelocation)
   for page in range(pdf.getNumPages()):
     pdf_writer = PdfFileWriter()
@@ -26,3 +33,10 @@ def iterate_over_file(filelocation):
     
     with open(app.config['UPLOAD_FOLDER'] + filename + '/' + output_filename, 'wb') as out:
       pdf_writer.write(out)
+    
+    #zipped_pdfs.write(app.config['UPLOAD_FOLDER'] + filename + '/' + output_filename)
+  
+  #zipped_pdfs.close()
+      
+def zip_split_files(fileslocation):
+  return
