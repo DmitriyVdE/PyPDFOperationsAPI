64aef3626beab7fcfc49e09010e7ee6064a7c83b
Dmitriy Van der Elst <dmitriy.vde@gmail.com>
2020-03-12 12:59:34 +0100

    Things for google cloud

--- API/google_cloud_storage_manager.py
+++ API/google_cloud_storage_manager.py
@@ -12,8 +12,14 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     blob.upload_from_filename(source_file_name)
 
-    print(
-        "File {} uploaded to {}.".format(
-            source_file_name, destination_blob_name
-        )
-    )
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
